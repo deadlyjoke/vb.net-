563763256b28329a9f5faed35a7231ebfa41887c
massi <massi@WS-MASSI>
2025-07-24 16:18:37 +0800

    加入專案檔案。

---
+++ Ex1/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblInput = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.btnCalculate.Location = New System.Drawing.Point(220, 274)
+        Me.btnCalculate.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(77, 37)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "計算"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.Label1.Location = New System.Drawing.Point(198, 35)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 27)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "計算階乘n!"
+        '
+        'lblInput
+        '
+        Me.lblInput.AutoSize = True
+        Me.lblInput.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.lblInput.Location = New System.Drawing.Point(90, 131)
+        Me.lblInput.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblInput.Name = "lblInput"
+        Me.lblInput.Size = New System.Drawing.Size(119, 16)
+        Me.lblInput.TabIndex = 2
+        Me.lblInput.Text = "請輸入一整數："
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(251, 124)
+        Me.txtInput.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(116, 23)
+        Me.txtInput.TabIndex = 3
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.lblResult.Location = New System.Drawing.Point(90, 210)
+        Me.lblResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(71, 16)
+        Me.lblResult.TabIndex = 4
+        Me.lblResult.Text = "結果值："
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(251, 210)
+        Me.txtResult.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(116, 23)
+        Me.txtResult.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(547, 380)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Font = New System.Drawing.Font("新細明體", 10.0!)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblInput As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents txtResult As TextBox
+End Class
